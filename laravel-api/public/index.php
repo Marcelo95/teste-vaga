ea3b9f2f4d3b09c4a71ee604ff7dcf8f0fbc6963
Marcelo <marcelovieira1995@gmail.com>
2021-11-16 16:07:56 -0300

    voltando

--- laravel-api/public/index.php
+++ laravel-api/public/index.php
@@ -3,7 +3,7 @@
 
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+
 
 define('LARAVEL_START', microtime(true));
 
@@ -49,11 +49,6 @@ require __DIR__.'/../vendor/autoload.php';
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
-
-if(DB::connection()->getDatabaseName())
-{
-   echo "Connected to database ".DB::connection()->getDatabaseName();
-}
 
 $response = $kernel->handle(
     $request = Request::capture()
