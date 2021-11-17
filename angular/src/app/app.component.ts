7f090df4a409827ec9d1ad3aef2e4e141668d962
Marcelo <marcelovieira1995@gmail.com>
2021-11-17 09:35:31 -0300

    Front(FEAT) - list

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,5 +1,17 @@
 import { Component } from '@angular/core';
 
+export interface New {
+  id : number;
+  title: string;
+  url : string,
+  image_small:string,
+  image_full:string,
+  resume: string;
+  date_formated: string;
+  category_slug : string;
+  text: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +19,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular';
+  news: New[] = [{
+    id: 1,
+    title : "Teset lorema sadjhsa sadkjdhskahds",
+    url : "",
+    image_small: "",
+    image_full: "",
+    resume : "Teset",
+    date_formated : "Teset",
+    category_slug : "Teset",
+    text : "Teset",
+  },{
+    id: 2,
+    title : "Teset2",
+    url : "",
+    image_small: "",
+    image_full: "",
+    resume : "Teset2",
+    date_formated : "Teset2",
+    category_slug : "Teset2",
+    text : "Teset2",
+  },{
+    id: 3,
+    title : "Teset2",
+    url : "",
+    image_small: "",
+    image_full: "",
+    resume : "Teset2",
+    date_formated : "Teset2",
+    category_slug : "Teset2",
+    text : "Teset2",
+  }];
+
+
+
+
+
+
 }
